7abad7df319363e751387b3e61ef94795591ac42
Muhammad Danish <danishkhilji112@gmail.com>
2024-04-25 15:16:55 -0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,8 +6,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import "@/styles/globals.css";
 //****Components****
 import Loader from "@/components/Shared/Loader";
-import Layout from "@/components/shared/Layout/index.jsx";
-
+import Layout from "../components/Shared/Layout/index.jsx";
 export default function App({ Component, pageProps: { ...pageProps } }) {
   const [loading, setLoading] = useState(false);
   const router = useRouter();
