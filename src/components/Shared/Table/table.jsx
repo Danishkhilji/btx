68671f717d5a8f978b9f1c6a662c639e23aeef9f
DanishKhilji <danishkhilji112@gmail.com>
2024-04-27 01:50:13 +0500

    update table

--- src/components/Shared/Table/table.jsx
+++ src/components/Shared/Table/table.jsx
@@ -58,14 +58,14 @@ const Table = () => {
                         <th scope="col" class="px-6 py-3">
                             Name
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        {/* <th scope="col" class="px-6 py-3">
                             Address
                         </th>
                         <th scope="col" class="px-6 py-3">
                             Image
-                        </th>
+                        </th> */}
                         <th scope="col" class="px-6 py-3">
-                            Toggel
+                            Stauts
                         </th>
                     </tr>
                 </thead>
@@ -75,18 +75,18 @@ const Table = () => {
                             <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {item.name}
                             </td>
-                            <td class="px-6 py-4">
+                            {/* <td class="px-6 py-4">
                                 {item.address}
                             </td>
                             <td class="px-6 py-4">
                                 <img src={item.imageUrl} alt="Profile Image" style={{ width: "50px", height: "auto" }} />
-                            </td>
+                            </td> */}
                             <td class="px-6 py-4">
                                 <label class="inline-flex items-center me-5 cursor-pointer">
                                     <input type="checkbox" value="" class="sr-only peer" checked={item.status || false}
                                         onChange={() => handleToggle(item._id, item.status)} />
                                     <div class="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-red-300 dark:peer-focus:ring-red-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-600"></div>
-                                    <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Red</span>
+                                    {/* <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Red</span> */}
                                 </label>
 
                             </td>
